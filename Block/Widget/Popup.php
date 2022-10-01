14e04714c7a44ee2a925784a712d167bbe93da24
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2022-10-01 16:36:09 -0300

    IN-277 | IN-280 | Release 1.0.0

---
+++ Block/Widget/Popup.php
@@ -0,0 +1,298 @@
+<?php
+/**
+ *
+ * @description Popup widget block
+ *
+ * @author Bina Commerce      <https://www.binacommerce.com>
+ * @author C. M. de Picciotto <cmdepicciotto@binacommerce.com>
+ *
+ */
+namespace Bina\Popup\Block\Widget;
+
+use Magento\Widget\Block\BlockInterface;
+use Magento\Cms\Block\Widget\Block;
+
+class Popup extends Block implements BlockInterface
+{
+    /**
+     *
+     * @var string
+     *
+     */
+    protected $_template = "widget/popup.phtml";
+
+    /**
+     *
+     * Get title
+     *
+     * @return string
+     *
+     */
+    public function getTitle()
+    {
+        return $this->getData('title');
+    }
+
+    /**
+     *
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return $this
+     *
+     */
+    public function setTitle($title)
+    {
+        return $this->setData('title', $title);
+    }
+
+    /**
+     *
+     * Get block ID
+     *
+     * @return string|int
+     *
+     */
+    public function getBlockId()
+    {
+        return $this->getData('block_id');
+    }
+
+    /**
+     *
+     * Set block ID
+     *
+     * @param string|int $blockId
+     *
+     * @return $this
+     *
+     */
+    public function setBlockId($blockId)
+    {
+        return $this->setData('block_id', $blockId);
+    }
+
+    /**
+     *
+     * Get show on init
+     *
+     * @return bool
+     *
+     */
+    public function getShowOnInit()
+    {
+        return $this->getData('show_on_init');
+    }
+
+    /**
+     *
+     * Set show on init
+     *
+     * @param bool $showOnInit
+     *
+     * @return $this
+     *
+     */
+    public function setShowOnInit($showOnInit)
+    {
+        return $this->setData('show_on_init', $showOnInit);
+    }
+
+    /**
+     *
+     * Get button title
+     *
+     * @return string|null
+     *
+     * @note The button is only shown if the popup is not shown on page initialization (because, in this case, the popup is shown on button click)
+     *
+     */
+    public function getButtonTitle()
+    {
+        return $this->getData('button_title');
+    }
+
+    /**
+     *
+     * Set button title
+     *
+     * @param string $buttonTitle
+     *
+     * @return $this
+     *
+     * @note The button is only shown if the popup is not shown on page initialization (because, in this case, the popup is shown on button click)
+     *
+     */
+    public function setButtonTitle($buttonTitle)
+    {
+        return $this->setData('button_title', $buttonTitle);
+    }
+
+    /**
+     *
+     * Get button ID
+     *
+     * @return string
+     *
+     * @note The button is only shown if the popup is not shown on page initialization (because, in this case, the popup is shown on button click)
+     *
+     */
+    public function getButtonId()
+    {
+        /**
+         *
+         * @note Check if button ID is set
+         *
+         */
+        if (!$this->getData('button_id')) {
+            /**
+             *
+             * @note If button ID is not set then generate a unique button ID
+             *
+             */
+            $buttonId = sprintf('popup_button_%s', uniqid());
+
+            /**
+             *
+             * @note Set generated button ID
+             *
+             */
+            $this->setButtonId($buttonId);
+        }
+
+        /**
+         *
+         * @note Return button ID
+         *
+         */
+        return $this->getData('button_id');
+    }
+
+    /**
+     *
+     * Set button ID
+     *
+     * @param string $buttonId
+     *
+     * @return $this
+     *
+     * @note The button is only shown if the popup is not shown on page initialization (because, in this case, the popup is shown on button click)
+     *
+     */
+    public function setButtonId($buttonId)
+    {
+        return $this->setData('button_id', $buttonId);
+    }
+
+    /**
+     *
+     * Get element ID
+     *
+     * @return string
+     *
+     */
+    public function getElementId()
+    {
+        /**
+         *
+         * @note Check if element ID is set
+         *
+         */
+        if (!$this->getData('element_id')) {
+            /**
+             *
+             * @note If element ID is not set then generate a unique element ID
+             *
+             */
+            $elementId = sprintf('popup_%s', uniqid());
+
+            /**
+             *
+             * @note Set generated element ID
+             *
+             */
+            $this->setElementId($elementId);
+        }
+
+        /**
+         *
+         * @note Return element ID
+         *
+         */
+        return $this->getData('element_id');
+    }
+
+    /**
+     *
+     * Set element ID
+     *
+     * @param string $elementId
+     *
+     * @return $this
+     *
+     */
+    public function setElementId($elementId)
+    {
+        return $this->setData('element_id', $elementId);
+    }
+
+    /**
+     *
+     * Get custom content HTML
+     *
+     * @return string|null
+     *
+     */
+    public function getCustomContentHtml()
+    {
+        return $this->getData('custom_content_html');
+    }
+
+    /**
+     *
+     * Set custom content HTML
+     *
+     * @param string $customContentHtml
+     *
+     * @return $this
+     *
+     */
+    public function setCustomContentHtml($customContentHtml)
+    {
+        return $this->setData('custom_content_html', $customContentHtml);
+    }
+
+    /**
+     *
+     * Get content HTML
+     *
+     * @return string
+     *
+     */
+    public function getContentHtml()
+    {
+        /**
+         *
+         * @note Check if custom content HTML is set
+         *
+         */
+        if ($customHtml = $this->getCustomContentHtml()) {
+            /**
+             *
+             * @note Return custom content HTML
+             *
+             */
+            return $customHtml;
+        }
+
+        /**
+         *
+         * @note Return CMS block text/HTML as content
+         *
+         * @see _beforeToHtml()
+         *
+         */
+        return $this->getText();
+    }
+}
